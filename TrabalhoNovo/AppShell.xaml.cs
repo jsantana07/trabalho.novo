dea0d6a5ab5f4152b2b8112eaa0429a98173d982
julia <jsantana23julia@gmail.com>
2024-09-17 11:50:42 -0300

    17/09

---
+++ TrabalhoNovo/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace TrabalhoNovo;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ TrabalhoNovo/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace TrabalhoNovo;
+
+public partial class MainPage : ContentPage
+{
+	
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	
+}
+
