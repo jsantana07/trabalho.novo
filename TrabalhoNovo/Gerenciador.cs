68c8526d6f494070b1764a79daded5287f169640
julia <jsantana23julia@gmail.com>
2024-09-19 09:03:23 -0300

    lkmjkj

---
+++ TrabalhoNovo/Gerenciador.cs
@@ -0,0 +1,17 @@
+namespace TrabalhoNovo
+{
+    public class Gerenciador
+    {
+        List <Questao> ListaQuestoes=new List <Questao> ();
+        List <int> ListaQuestoesRespondidas=new List<int> ();
+        Questao QuestaoCorrente;
+
+        public Gerenciador(Label labelPerg, Button)
+         {
+            CriaPerguntas()
+            
+         }
+
+
+    }
+}
